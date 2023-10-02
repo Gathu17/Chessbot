af247ec7a7742c4f2cdee940c97b05dd2fa90cfd
Jerry <jjgathu17@gmail.com>
2023-10-02 10:00:44 +0300

    remove unneeded files

--- src/utils/chessPosConverter.js
+++
@@ -1,6 +0,0 @@
-export function positionToChessNotation(position) {
-    const row = Math.floor(position / 10);
-    const col = String.fromCharCode((position % 10) + 97);
-    return `${row}${col}`;
-  }
-  
