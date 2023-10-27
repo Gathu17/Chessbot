099615850c7b5ca42c5cf58d2839520c9b8fc70f
Michael <michael-maina@yashio-corp.com>
2023-10-27 05:21:00 +0300

    added player names and integrated them into opponents

--- client/src/components/play/ChessBoard.jsx
+++ client/src/components/play/ChessBoard.jsx
@@ -19,6 +19,7 @@ const ChessBoard = ({
   blackCountdown,
   setBlackCountdown,
   setWhiteCountdown,
+  level
 }) => {
   const board = React.useRef(null);
   const squares = Array(64)
@@ -272,12 +273,16 @@ const ChessBoard = ({
   };
   function botPlay() {
     const positionMoved = [];
+    const prevTime = Date.now();
+    const times = [1000, 2000];
+    const randTime = times[Math.floor(Math.random() * 2)]
+    
     if (playBot.state && turnPlaying == playBot.color) {
       console.log("bot play");
       setTimeout(() => {
-        positionMoved.push(...game.makeBestMove(playBot.color, setBlackCountdown));
+        positionMoved.push(...game.makeBestMove(playBot.color, setBlackCountdown, prevTime, level));
         updateRep('white', positionMoved);
-      }, 100);
+      }, randTime);
     }
   }
 
